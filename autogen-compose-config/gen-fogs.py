a45a4a6c6c063b04776d776023e9bafb34d1f904
Ishan Sharma <mailmeishan007@gmail.com>
2019-08-02 19:18:11 +0530

    execute tc rules during container startup

--- autogen-compose-config/gen-fogs.py
+++ autogen-compose-config/gen-fogs.py
@@ -14,16 +14,16 @@ def initializeComposeFile(fileName):
 
 def writeFog(id, ip, port, fogFileName):
     file = open(fogFileName,'a',encoding='utf-8')
-    fogCommand  = "java -Xms80m -Xmx800m -cp edgefs/cli/target/edgefilesystem-0.1-jar-with-dependencies.jar com.dreamlab.edgefs.controlplane.FogServer {} {} 1 1 0.25 0".format(ip,str(port))
-    ttext = "  fog{}:\n    container_name: fog{}\n    image: elfstorenojson\n    mem_limit: 4096m\n    cpus: 2\n    volumes:\n      - ./Logs/serverLogs/:/log.dir_IS_UNDEFINED/\n    networks:\n      default:\n        ipv4_address: {}\n    expose:\n      - \"{}\"\n    command: {}\n\n".format(str(id),str(id),ip,port,fogCommand)
+    fogCommand  = "\"source tc_rule.sh && java -Xms80m -Xmx800m -cp edgefs/cli/target/edgefilesystem-0.1-jar-with-dependencies.jar com.dreamlab.edgefs.controlplane.FogServer {} {} 1 1 0.25 0\"".format(ip,str(port))
+    ttext = "  fog{}:\n    container_name: fog{}\n    image: elfstorenojson\n    mem_limit: 4096m\n    cpus: 2\n    volumes:\n      - ./Logs/serverLogs/:/log.dir_IS_UNDEFINED/\n    networks:\n      default:\n        ipv4_address: {}\n    expose:\n      - \"{}\"\n    command: bash -c {}\n\n".format(str(id),str(id),ip,port,fogCommand)
     file.write(ttext)
     #print(ttext)
     file.close()
 
 def writeEdge(edgeId,edgeIp,edgePort,fogIp,fogPort,edgeFileName):
     file  = open(edgeFileName,'a',encoding='utf-8')
-    edgeCommand = "java -Xms40m -Xmx200m -cp edgefs/cli/target/edgefilesystem-0.1-jar-with-dependencies.jar com.dreamlab.edgefs.edge.server.EdgeServer {} {} {} 88 {} {}  /edgefs/data  /edgefs/logs".format(edgeId,edgeIp,edgePort,fogIp,fogPort)
-    ttext  = "  edge{}:\n    container_name: edge{}\n    image: elfstorenojson\n    mem_limit: 4096m\n    cpus: 1\n    volumes:\n      - ./Logs/edgeClientLogs/:/edgefs/logs/\n      - ./Logs/edgeServerLogs/:/log.dir_IS_UNDEFINED/\n    networks:\n      default:\n       ipv4_address: {}\n    expose:\n      - \"{}\"\n    command: {}\n\n".format(str(edgeId),str(edgeId),edgeIp,str(edgePort),edgeCommand)
+    edgeCommand = "\"source tc_rule.sh && java -Xms40m -Xmx200m -cp edgefs/cli/target/edgefilesystem-0.1-jar-with-dependencies.jar com.dreamlab.edgefs.edge.server.EdgeServer {} {} {} 88 {} {}  /edgefs/data  /edgefs/logs\"".format(edgeId,edgeIp,edgePort,fogIp,fogPort)
+    ttext  = "  edge{}:\n    container_name: edge{}\n    image: elfstorenojson\n    mem_limit: 4096m\n    cpus: 1\n    volumes:\n      - ./Logs/edgeClientLogs/:/edgefs/logs/\n      - ./Logs/edgeServerLogs/:/log.dir_IS_UNDEFINED/\n    networks:\n      default:\n       ipv4_address: {}\n    expose:\n      - \"{}\"\n    command: bash -c {}\n\n".format(str(edgeId),str(edgeId),edgeIp,str(edgePort),edgeCommand)
     file.write(ttext)
     #print(ttext)
     file.close()
